1bb75d3c976edf04750968f12fa44918da221568
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-05 10:50:41 +0530

    Add files via upload
    Here the code goes with astricts

---
+++ nestedforuse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int x;
+
+ int main(){
+ 	cout << "Enter number" << endl;
+ 	cin >> x ;
+ 	for (int i=0; i<x; i++){
+ 		for(int j=0; j<i ;j++){
+ 			cout << "*";
+ 		}
+ 		cout << endl;
+ 	}
+ }
+ //FOM 35 vidurathegeek

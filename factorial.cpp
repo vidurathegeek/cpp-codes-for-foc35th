ffe2d99581969bc09fe832b048fad5d94dea3964
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-05 10:19:13 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,18 +1,17 @@
-// #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 int num;
-double fact = 1;
+unsigned long long fact = 1; // to check number more than 50
 int main(){
-	//printf("Enter any number \n");
 	cout << "Enter any number" << endl;
 	cin >> num;
-	//scanf("%d", &num);
 	for(int i=1; i<=num; i++){
 		fact = fact * i;
 	}
-	printf(" %d \n",fact );
+	//printf(" %d \n",fact );
+	//cout.setf(ios::fixed);
+	//cout.precision(0);
 	cout << fact << endl;
 }
 // FOM 35th vidurathegeek

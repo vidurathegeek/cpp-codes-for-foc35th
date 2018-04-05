017af1db223a3780a879019777797390cc176d5f
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-05 10:56:00 +0530

    Update nestedforuse.cpp
    Updated for both questions

--- nestedforuse.cpp
+++ nestedforuse.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
- int x;
+int x;
 
- int main(){
+int main(){
  	cout << "Enter number" << endl;
  	cin >> x ;
- 	for (int i=0; i<x; i++){
+ 	for (int i=1; i<=x; i++){
  		for(int j=0; j<i ;j++){
  			cout << "*";
  		}
  		cout << endl;
  	}
- }
+ 	for(int k=0; k<x ; k++){
+ 		for(int o=1 ; o<=k ; o++){
+ 			cout << o;
+ 		}
+ 		cout << endl;
+ 	}
+}
  //FOM 35 vidurathegeek
+

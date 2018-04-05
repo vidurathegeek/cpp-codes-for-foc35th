67e81103fa1a6d6e3428d4173db5fe9b4d2ffe72
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-05 09:54:24 +0530

    Here the factorial code

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int num;
+int fact = 1;
+int main(){
+	printf("Enter any number \n");
+	scanf("%d", &num);
+	for(int i=1; i<=num; i++){
+		fact = fact * i;
+	}
+	printf(" %d \n",fact );
+}

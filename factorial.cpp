91dc75a44f4d7bc529ef137fccc01433ccb2d844
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-05 10:11:08 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,13 +1,18 @@
-#include <stdio.h>
+// #include <stdio.h>
+#include <iostream>
 
+using namespace std;
 int num;
 double fact = 1;
 int main(){
-	printf("Enter any number \n");
-	scanf("%d", &num);
+	//printf("Enter any number \n");
+	cout << "Enter any number" << endl;
+	cin >> num;
+	//scanf("%d", &num);
 	for(int i=1; i<=num; i++){
 		fact = fact * i;
 	}
 	printf(" %d \n",fact );
+	cout << fact << endl;
 }
 // FOM 35th vidurathegeek

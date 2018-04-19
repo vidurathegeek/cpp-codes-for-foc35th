b6d7074027904c1bd6b61e453da4ea9f34bdd0f5
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-19 08:50:06 +0530

    Add files via upload
    here the sum till 999 code

---
+++ sumtill999.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int x,nx;
+int tot;
+
+int main(){
+	while (true){
+		cin >> x;
+		while (x!=999){
+			tot+=x;
+			cin >> x;
+		}
+		cout << tot << endl;
+	}	
+}

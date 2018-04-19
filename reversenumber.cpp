1bac20b24a11f99c1647c5f856fea2fa20121a08
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-19 09:50:24 +0530

    Add files via upload
    Here the Palindrome code

---
+++ reversenumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int x,xn,revno=0,n;
+
+int main(){
+	cin >> xn;
+	x=xn;
+	while(x!=0){
+		n = x%10;
+	    revno = revno*10 + n;
+		x=x/10;	
+	}
+    cout << revno << endl;
+    cout << xn << endl;
+    if (xn==revno){
+    	cout << "It's a Palindrome" << endl;
+	}
+	else {
+		cout << "It's not a Palindrome" << endl;
+	}
+}

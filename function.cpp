dbad4fdaed26e288eeb3cc192e936fc4707921fe
Vidura Dhananjaya <viduradtej@gmail.com>
2018-04-26 10:17:25 +0530

    Update function cpp
    Check that 2nd comment, there you'll get the code you need to write function with a return value to main function.

--- function.cpp
+++ function.cpp
@@ -17,3 +17,25 @@ void printTotal(int x, int y){
 	cout << "Total is " << tot << endl;
 }
 // FOC 35th vidurathegeek
+/*
+#include <iostream>
+
+using namespace std;
+
+int x,y,tot;
+int printTotal(int x,int y);
+
+int main(){
+	cin >> x;
+	cin >> y;
+	printTotal(x,y);
+	cout << tot << endl;
+
+}
+
+int printTotal(int x,int  y){
+	tot = x+y;
+	return tot;
+}
+*/
+// here the 2nd one is the function with return value thing.

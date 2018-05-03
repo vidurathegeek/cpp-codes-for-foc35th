3d3d660c09a555bd596a833e3fd2d7107cfb710d
Vidura Dhananjaya <viduradtej@gmail.com>
2018-05-03 09:11:50 +0530

    Add files via upload

---
+++ maxminarray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int marks[10];
+int maxm=0;
+
+int main(){
+	for(int i=0;i<10;i++){
+		cin >> marks[i];
+	}
+	int min =marks[0];
+	for(int i=0;i<10;i++){
+		if(marks[i]>maxm){
+			maxm = marks[i];
+		}
+		if(marks[i]<min){
+			min = marks[i];
+		}
+	}
+	cout << min << endl;
+	cout << maxm << endl;
+}
